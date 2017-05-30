3b13756dd8d6a00e9177e9f5769cca5a42b64c76
Silvan Jegen <silvan@smallpdf.com>
2017-05-30 11:47:45 +0200

    Add Radius to the DrawingInstruction

--- drawinginstruction.go
+++ drawinginstruction.go
@@ -17,10 +17,14 @@ const (
 
 // DrawingInstruction contains enough information that a simple drawing
 // library can draw the shapes contained in an SVG file.
+//
+// The struct contains all necessary fields but only the ones needed (as
+// indicated byt the InstructionType) will be non-nil.
 type DrawingInstruction struct {
-	Kind InstructionType
-	M    *Tuple
-	C1   *Tuple
-	C2   *Tuple
-	T    *Tuple
+	Kind   InstructionType
+	M      *Tuple
+	C1     *Tuple
+	C2     *Tuple
+	T      *Tuple
+	Radius *float64
 }
